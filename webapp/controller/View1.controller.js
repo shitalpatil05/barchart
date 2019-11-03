7b84db4f36e701394a1093e7a9cbb02908ce1df9
SHITAL PATIL <shital.patil@invenio-solutions.com>
2019-11-03 14:53:27 +0000

    My First Commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("demo.ui5_barchart.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
